27bc06270385d8bf8ec06875ca7437dcd42b0461
makfive <makihiko.kuwano@gmail.com>
2014-06-09 22:32:06 +0900

    Initial commit

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,21 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
+require 'securerandom'
+
+def secure_token
+  token_file = Rails.root.join('.secret')
+  if File.exist?(token_file)
+    # Use the existing token.
+    File.read(token_file).chomp
+  else
+    # Generate a new token and store it in token_file.
+    token = SecureRandom.hex(64)
+    File.write(token_file, token)
+    token
+  end
+end
+
+
+
 Spark::Application.config.secret_key_base = '8c4fd395eeda3962a97a0243c7608e697151431b36c940a6fca5f5202c04f43f121cb7a3df98741f8e52850267efa84bc384092833e040cd62be75ad4a47caf9'
